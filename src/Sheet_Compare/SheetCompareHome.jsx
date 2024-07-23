ee3faa1e4b7f2572b84941234442fac4464a4363
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2024-07-23 15:43:14 -0400

    Note on Working Section of Page

--- src/Sheet_Compare/SheetCompareHome.jsx
+++ src/Sheet_Compare/SheetCompareHome.jsx
@@ -5,6 +5,12 @@ export default function SheetCompareHome({ setViewPage }) {
   return (
     <div>
       <Header setViewPage={setViewPage} />
+
+      <div className="p-3 flex flex-col justify-center items-center">
+        <p className="text-xl font-bold">{`Compare two Excel Sheets => Output: Compareison Data`}</p>
+        <p className="text-xl font-bold">BUILD IN PROGRESS...</p>
+        <p className="font-bold">*** Click Sunbird Logo to go Home ***</p>
+      </div>
     </div>
   );
 }
